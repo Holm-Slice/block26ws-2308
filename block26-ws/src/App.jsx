a526f8be45d32977bd6f84139b96be4d33683e9c
Holm-Slice <taylorholm92@gmail.com>
2023-11-12 16:17:20 -0600

    idk

---
+++ block26-ws/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import ContactList from '../components/Contact-List'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <ContactList/>
+    </>
+  )
+}
+
+export default App
